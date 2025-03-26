1ba0d33eea8eb4d633763c4d9db2e2a1a23a0508
Choi MinJung <mj020503@naver.com>
2025-03-26 14:34:16 +0900

    Initial Setting

---
+++ src/test/java/BE_Elixir/Elixir/ElixirApplicationTests.java
@@ -0,0 +1,13 @@
+package BE_Elixir.Elixir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElixirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
