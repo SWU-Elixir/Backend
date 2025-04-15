7f7baab598dcfd4bded9c29ce157a96d4ab1482b
seoyeon83 <mool848777@naver.com>
2025-04-16 01:33:52 +0900

    feat: Security 설정 업데이트

---
+++ src/main/java/BE_Elixir/Elixir/global/security/SecurityConfig.java
@@ -0,0 +1,104 @@
+package BE_Elixir.Elixir.global.security;
+
+import BE_Elixir.Elixir.global.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.PrintWriter;
+import java.nio.file.AccessDeniedException;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // 권한 확인을 하지 않는 url
+    private static final String[] PERMIT_ALL_PATTERNS = new String[] {
+            "/test",
+            "/swagger-ui/*",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/api-docs/**",
+            "/api/member/check-email",
+            "/api/member/signup",
+            "/api/auth/login"
+    };
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080", "http://localhost:8080/*");
+            }
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                // REST API 이기 떄문에 basic auth, csrf 보안 사용 X
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .headers((headerConfig) -> headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                // jwt 사용 -> 세션 XX
+                .sessionManagement(configurer -> configurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // 검사하지 않는 url
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PERMIT_ALL_PATTERNS).permitAll()
+                        .anyRequest().authenticated())
+                // jwt 인증을 위해 직접 구현한 필터
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling((exceptionConfig) ->
+                        exceptionConfig.authenticationEntryPoint(unauthorizedEntryPoint).accessDeniedHandler(accessDeniedHandler)
+                ).getOrBuild();   // 401, 403 관련 예외처리
+    }
+
+    // 인증 실패 처리
+    private final AuthenticationEntryPoint unauthorizedEntryPoint =
+            ((request, response, authException) -> {
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                String json = new ObjectMapper().writeValueAsString(ErrorCode.UNAUTHORIZED);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                PrintWriter writer = response.getWriter();
+                writer.write(json);
+                writer.flush();
+            });
+
+    // 인가 실패 처리
+    private final AccessDeniedHandler accessDeniedHandler =
+            ((request, response, accessDeniedException) -> {
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                String json = new ObjectMapper().writeValueAsString(ErrorCode.FORBIDDEN);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                PrintWriter writer = response.getWriter();
+                writer.write(json);
+                writer.flush();
+            });
+}
