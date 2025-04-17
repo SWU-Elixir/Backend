a68a38adf68ef2e9487437f1960910f154fa607c
seoyeon83 <mool848777@naver.com>
2025-04-17 16:31:33 +0900

    feat: 프론트 요청 사항에 맞춰 설문조사 문항별 문자열 배열로 값을 받도록 수정 및 필드명 CamelCase 방식으로 변경

--- src/main/java/BE_Elixir/Elixir/domain/member/dto/SignUpRequestDTO.java
+++ src/main/java/BE_Elixir/Elixir/domain/member/dto/SignUpRequestDTO.java
@@ -17,62 +17,24 @@ public class SignUpRequestDTO {
     private String email;
     private String password;
     private String nickname;
-    private String profile_url;
+    private String profileUrl;
     private String gender;
-    private Integer birth_year;
+    private Integer birthYear;
 
-    // allergy fields
-    private boolean allergy_egg;
-    private boolean allergy_milk;
-    private boolean allergy_grain;
-    private boolean allergy_wheat_product;
-    private boolean allergy_dairy;
-    private boolean allergy_buckwheat;
-    private boolean allergy_peanut;
-    private boolean allergy_soybean;
-    private boolean allergy_wheat;
-    private boolean allergy_mackerel;
-    private boolean allergy_pork;
-    private boolean allergy_peach;
-    private boolean allergy_tomato;
-    private boolean allergy_sulfite;
-    private boolean allergy_walnut;
-    private boolean allergy_chicken;
-    private boolean allergy_beef;
-    private boolean allergy_squid;
-    private boolean allergy_shellfish;
-    private boolean allergy_oyster;
-    private boolean allergy_abalone;
-    private boolean allergy_mussel;
-    private boolean allergy_pine_nut;
+    private List<String> allergies;
+    private List<String> mealStyles;
+    private List<String> recipeStyles;
+    private List<String> reasons;
 
-    // meal style fields
-    private boolean meal_style_meat_based;
-    private boolean meal_style_vegetable_based;
-    private boolean meal_style_mixed;
-
-    // recipe style fields
-    private boolean recipe_style_korean;
-    private boolean recipe_style_chinese;
-    private boolean recipe_style_japanese;
-    private boolean recipe_style_western;
-    private boolean recipe_style_dessert;
-    private boolean recipe_style_beverage_tea;
-    private boolean recipe_style_sauce_jam;
-
-    // reason fields
-    private boolean reason_antioxidant_boost;
-    private boolean reason_blood_sugar_control;
-    private boolean reason_inflammation_reduction;
 
     public Member toEntity(String encodedPassword, List<String> roles) {
         return Member.builder()
                 .email(this.email)
                 .password(encodedPassword)
                 .nickname(this.nickname)
-                .profile_url(this.profile_url)
+                .profileUrl(this.profileUrl)
                 .gender(this.gender)
-                .birth_year(this.birth_year)
+                .birthYear(this.birthYear)
                 .roles(roles)
                 .build();
     }
