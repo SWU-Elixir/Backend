19cfee6105750e9dd2085b6ca5bff3b02699d942
Choi MinJung <mj020503@naver.com>
2025-05-16 14:52:25 +0900

    refactor: 배포 URL을 환경설정 파일에서 주입받도록 변경

--- src/main/java/BE_Elixir/Elixir/global/config/SwaggerConfig.java
+++ src/main/java/BE_Elixir/Elixir/global/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,7 +21,8 @@ import java.util.List;
 )
 public class SwaggerConfig {
 
-    String DEFAULT_URL = "https://port-0-elixir-backend-g0424l70py8py.gksl2.cloudtype.app/";
+    @Value("${swagger.app.default-url}")
+    private String DEFAULT_URL;
 
     @Bean
     public OpenAPI openAPI() {
