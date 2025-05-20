c9080edbf79e49af364da373eb253e368446fcb3
seoyeon83 <mool848777@naver.com>
2025-05-20 16:42:21 +0900

    feat: id 목록으로 식재료명 목록만 조회, 챗봇 기능에 필요

--- src/main/java/BE_Elixir/Elixir/domain/ingredient/repository/IngredientRepository.java
+++ src/main/java/BE_Elixir/Elixir/domain/ingredient/repository/IngredientRepository.java
@@ -2,6 +2,8 @@ package BE_Elixir.Elixir.domain.ingredient.repository;
 
 import BE_Elixir.Elixir.domain.ingredient.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +15,8 @@ public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
     // 당월 챌린지 식재료 조회
     List<Ingredient> findByChallengeMonth(int month);
+
+    // ID로 찾기
+    @Query("SELECT i.name FROM Ingredient i WHERE i.id IN :ids")
+    List<String> findNamesByIds(@Param("ids") List<Long> ids);
 }
