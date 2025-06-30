6daa74aaae96cd61939ced663cb6bf29e0fdf60b
seoyeon83 <mool848777@naver.com>
2025-06-30 21:38:41 +0900

    feat: 메인 메소드에 타임존 설정 추가

--- src/main/java/BE_Elixir/Elixir/ElixirApplication.java
+++ src/main/java/BE_Elixir/Elixir/ElixirApplication.java
@@ -3,10 +3,16 @@ package BE_Elixir.Elixir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ElixirApplication {
 
 	public static void main(String[] args) {
+		// KST로 타임존 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+
 		SpringApplication.run(ElixirApplication.class, args);
 	}
 
