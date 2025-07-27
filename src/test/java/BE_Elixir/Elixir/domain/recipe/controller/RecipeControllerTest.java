82444166a2e6cd223447acf7959e93f49211b0f7
Choi MinJung <mj020503@naver.com>
2025-07-27 16:50:40 +0900

    test: 레시피 상세 조회 Controller 테스트 작성

---
+++ src/test/java/BE_Elixir/Elixir/domain/recipe/controller/RecipeControllerTest.java
@@ -0,0 +1,170 @@
+package BE_Elixir.Elixir.domain.recipe.controller;
+
+import BE_Elixir.Elixir.domain.ingredient.entity.Ingredient;
+import BE_Elixir.Elixir.domain.member.entity.MemberDetails;
+import BE_Elixir.Elixir.domain.recipe.entity.Material;
+import BE_Elixir.Elixir.domain.recipe.entity.RecipeIngredient;
+import org.junit.jupiter.api.Test;
+import BE_Elixir.Elixir.domain.member.entity.Member;
+import BE_Elixir.Elixir.domain.recipe.dto.response.RecipeDetailResponseDTO;
+import BE_Elixir.Elixir.domain.recipe.entity.Recipe;
+import BE_Elixir.Elixir.domain.recipe.repository.RecipeRepository;
+import BE_Elixir.Elixir.domain.recipe.service.RecipeService;
+import BE_Elixir.Elixir.global.enums.CategorySlowAging;
+import BE_Elixir.Elixir.global.enums.CategoryType;
+import BE_Elixir.Elixir.global.enums.Difficulty;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RecipeController.class)
+@MockitoBean(types = {RecipeService.class, RecipeRepository.class})
+public class RecipeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private RecipeService recipeService;
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @AfterEach
+    public void cleanUp() {
+        recipeRepository.deleteAll();
+    }
+    private Member createMockMember() {
+        Member member = mock(Member.class);
+        when(member.getId()).thenReturn(500L);
+        return member;
+    }
+
+    private Recipe createMockRecipe() {
+        Recipe recipe = mock(Recipe.class);
+        Member mockMember = createMockMember();
+
+        when(recipe.getMember()).thenReturn(mockMember);
+        when(recipe.getId()).thenReturn(500L);
+        when(recipe.getTitle()).thenReturn("테스트 레시피");
+        when(recipe.getImageUrl()).thenReturn("https://example.com/image.jpg");
+        when(recipe.getDescription()).thenReturn("테스트 설명");
+        when(recipe.getCategorySlowAging()).thenReturn(CategorySlowAging.항산화강화);
+        when(recipe.getCategoryType()).thenReturn(CategoryType.한식);
+        when(recipe.getDifficulty()).thenReturn(Difficulty.보통);
+        when(recipe.getTimeHours()).thenReturn(1);
+        when(recipe.getTimeMinutes()).thenReturn(30);
+
+        RecipeIngredient mockTag = mock(RecipeIngredient.class);
+        Ingredient mockIngredient = mock(Ingredient.class);
+        when(mockIngredient.getId()).thenReturn(10L);
+        when(mockTag.getIngredient()).thenReturn(mockIngredient);
+        when(recipe.getIngredientTags()).thenReturn(List.of(mockTag));
+
+        Material mockMaterial = mock(Material.class);
+        when(mockMaterial.getName()).thenReturn("재료/양념 1");
+        when(mockMaterial.getValue()).thenReturn("100");
+        when(mockMaterial.getUnit()).thenReturn("g");
+        when(recipe.getIngredients()).thenReturn(List.of(mockMaterial));
+        when(recipe.getSeasonings()).thenReturn(List.of(mockMaterial));
+
+        when(recipe.getStepDescriptions()).thenReturn(List.of("1단계 설명"));
+        when(recipe.getStepImageUrls()).thenReturn(List.of("https://example.com/step1.jpg"));
+        when(recipe.getTips()).thenReturn("팁");
+        when(recipe.getLikes()).thenReturn(10);
+        when(recipe.getCreatedAt()).thenReturn(LocalDateTime.now().minusDays(1));
+        when(recipe.getUpdatedAt()).thenReturn(LocalDateTime.now());
+        when(recipe.getAllergy_우유()).thenReturn(true);
+        when(recipe.getAllergy_밀()).thenReturn(true);
+        return recipe;
+    }
+
+    @Test
+    @DisplayName("레시피_등록")
+    void createRecipe() {
+    }
+
+    @Test
+    @DisplayName("레시피_상세_조회")
+    void getRecipe() throws Exception {
+        // given
+        String url = "/api/recipe/500";
+        Recipe recipe = createMockRecipe();
+
+        RecipeDetailResponseDTO dto = new RecipeDetailResponseDTO(
+                recipe,
+                true,
+                false,
+                false
+        );
+
+        given(recipeService.getRecipeDetail(eq(500L), any())).willReturn(dto);
+
+        Member mockMember = createMockMember();
+        MemberDetails memberDetails = new MemberDetails(mockMember);
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(memberDetails, null, List.of()));
+        SecurityContextHolder.setContext(context);
+
+
+        // when - API 호출
+        final ResultActions result = mockMvc.perform(get(url)
+                .with(SecurityMockMvcRequestPostProcessors.authentication(
+                        new UsernamePasswordAuthenticationToken(memberDetails, null, List.of()))
+                )
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.id").value(500L))
+                .andExpect(jsonPath("$.data.title").value("테스트 레시피"))
+                .andExpect(jsonPath("$.data.likes").value(10 ))
+                .andExpect(jsonPath("$.data.ingredients[0].name").value("재료/양념 1"))
+                .andExpect(jsonPath("$.data.allergies").value(org.hamcrest.Matchers.containsInAnyOrder("우유", "밀")));
+    }
+
+    @Test
+    void getRecipes() {
+    }
+
+    @Test
+    void getSearchRecipe() {
+    }
+
+    @Test
+    void getSearchKeyword() {
+    }
+
+    @Test
+    void updateRecipe() {
+    }
+
+    @Test
+    void deleteRecipe() {
+    }
+
+    @Test
+    void getMyRecipes() {
+    }
+}
