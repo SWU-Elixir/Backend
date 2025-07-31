6154b8fad031febb0726e6baa295129700c2f1d7
seoyeon83 <mool848777@naver.com>
2025-07-31 15:54:50 +0900

    feat: 생성자 어노테이션 추가

--- src/main/java/BE_Elixir/Elixir/domain/auth/dto/request/LoginRequestDTO.java
+++ src/main/java/BE_Elixir/Elixir/domain/auth/dto/request/LoginRequestDTO.java
@@ -1,7 +1,9 @@
 package BE_Elixir.Elixir.domain.auth.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class LoginRequestDTO {
     private String email;
